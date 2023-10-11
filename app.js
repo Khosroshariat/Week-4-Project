784a0703ba40f435c01178bfdc0ec37f41535d3e
khosro <khosroshariatzadeh@gmail.com>
2023-10-11 16:33:09 -0400

    3rd edit

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 
-const url = `https://www.omdbapi.com/?apikey=4d3b95cb&s=fast`
-const movieListEl = document.querySelector('.movie__loading')
-    
-async function main(filter){
-    
-    const data = await fetch(url)
+const movieListEl = document.querySelector('.movie__link')
+
+async function search(filter){
+
+    const title = document.querySelector('.search__input').value
+    const data = await fetch(`https://www.omdbapi.com/?apikey=4d3b95cb&s=${title || ""}`)
     const movieData = await data.json()
     const movieArray = movieData.Search.slice(0, 6)
-    
+
     if(filter === 'NEW_TO_OLD') {
-         movieArray.sort((a, b) => b.Year - a.Year)
+        movieArray.sort((a, b) => b.Year - a.Year)
     }
     if(filter === 'OLD_TO_NEW') {
-         movieArray.sort((a, b) => a.Year - b.Year)
+        movieArray.sort((a, b) => a.Year - b.Year)
     }
+
 
     movieListEl.innerHTML = movieArray.map((user) => getMovies(user)).join("")
 }
 
 function filterMovies(event) {
-    main(event.target.value)
+    search(event.target.value)
 }
 
-setTimeout (() => {
-    main()
+setTimeout(() => {
+    search()
 }, 1000)
-
 
 function getMovies(user) {
     return `<div class="movies_lists">               
@@ -38,13 +38,5 @@ function getMovies(user) {
     </div>`
 }
 
-async function search(event){
-    event.preventDefault()
-    const title = document.querySelector('.search__input').value
-    const data = await fetch(`https://www.omdbapi.com/?apikey=4d3b95cb&s=${title || ""}`)
-    const movieData = await data.json()
-    const movieArray = movieData.Search.slice(0, 6)
-    movieListEl.innerHTML = movieArray.map((user) => getMovies(user)).join("")
-}
 
 
