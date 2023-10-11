4f85c9b926bdf3370691636ad8c316d290b57682
khosro <khosroshariatzadeh@gmail.com>
2023-10-10 20:35:42 -0400

    1st edit

--- app.js
+++ app.js
@@ -3,22 +3,19 @@ const url = `https://www.omdbapi.com/?apikey=4d3b95cb&s=fast`
 const movieListEl = document.querySelector('.movie__loading')
     
 async function main(filter){
-
+    
     const data = await fetch(url)
     const movieData = await data.json()
-    const movieArray = movieData.Search
-    movieListEl.innerHTML = movieArray.map((user) => getMovies(user)).join("")
+    const movieArray = movieData.Search.slice(0,6)
     
     if(filter === 'NEW_TO_OLD') {
-        console.log(filter)
         const filterMoviess = movieArray.sort((a, b) => b.Year - a.Year)
-        console.log(filterMoviess)
     }
     if(filter === 'OLD_TO_NEW') {
-        console.log(filter)
         const filterMoviess = movieArray.sort((a, b) => a.Year - b.Year)
-        console.log(filterMoviess)
     }
+
+    movieListEl.innerHTML = movieArray.map((user) => getMovies(user)).join("")
 }
 
 function filterMovies(event) {
@@ -31,14 +28,14 @@ setTimeout (() => {
 
 
 function getMovies(user) {
-   return `<div class="movies_lists">               
-   <div class="movie__detail">
-     <img class="movie__img" src="${user.Poster}" alt="">
-      <h4 class="movie__details">Movie title: ${user.Title} </h4>
-      <h4 class="movie__details">Year: <span> ${user.Year} </span></h4>
-      <h4 class="movie__details">Type: <span> ${user.Type} </span></h4>
-   </div>
-</div>`
+    return `<div class="movies_lists">               
+    <div class="movie__detail">
+    <img class="movie__img" src="${user.Poster}" alt="">
+    <h4 class="movie__details"> ${user.Title} </h4>
+    <h5 class="movie__details">Year: <span> ${user.Year} </span></h5>
+    <h5 class="movie__details">Type: <span> ${user.Type} </span></h5>
+    </div>
+    </div>`
 }
 
 async function search(event){
@@ -46,7 +43,16 @@ async function search(event){
     const data = await fetch(`https://www.omdbapi.com/?apikey=4d3b95cb&s=${title || ""}`)
     const movieData = await data.json()
     const movieArray = movieData.Search.slice(0,6)
-    movieListEl.innerHTML = movieArray
-    .map((user) => getMovies(user)).join("")
+
+    if(filter === 'NEW_TO_OLD') {
+        const filterMoviess = movieArray.sort((a, b) => b.Year - a.Year)
+    }
+    if(filter === 'OLD_TO_NEW') {
+        const filterMoviess = movieArray.sort((a, b) => a.Year - b.Year)
+    }
+
+    movieListEl.innerHTML = movieArray.map((user) => getMovies(user)).join("")
+
 }
+
 
